dfb3cc83418e090a44305f3eaae0a6b24307c5c2
TommyZhang <zhanghq139@139.com>
2016-10-17 23:02:48 +0800

    增加sql文件

---
+++ DBProcess/FieldTest.sql
@@ -0,0 +1,40 @@
+USE [MyDB]
+GO
+
+/****** Object:  Table [dbo].[FieldTest]    Script Date: 2016/10/17 22:50:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[FieldTest](
+	[int1] [int] NULL,
+	[int2big] [bigint] NULL,
+	[int3small] [smallint] NULL,
+	[int4tiny] [tinyint] NULL,
+	[str1vchar] [varchar](50) NULL,
+	[str2char] [char](10) NULL,
+	[str3text] [text] NULL,
+	[dig1num] [numeric](10, 8) NULL,
+	[dig2dec] [decimal](20, 8) NULL,
+	[dig3float] [float] NULL,
+	[time1date] [date] NULL,
+	[time2dt] [datetime] NULL,
+	[time3stamp] [timestamp] NULL,
+	[guid] [uniqueidentifier] NULL,
+	[bin1] [binary](50) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[FieldTest] ADD  CONSTRAINT [DF_FieldTest_guid]  DEFAULT (newid()) FOR [guid]
+GO
+
+
