9a585104c4fff785f82517df662f460759ef06e8
Zhanghq <zhanghq139@139.com>
2016-11-02 18:03:24 +0800

    modified

--- AnimationButton/rotarybutton.h
+++ AnimationButton/rotarybutton.h
@@ -31,16 +31,24 @@ protected:
     void paintEvent(QPaintEvent *);
     
 private:    
+<<<<<<< HEAD
     int angle;                              //rotation angle
     
+=======
+>>>>>>> 291ec0a3dd14e83069641ba3098ad848463efc3f
     int pixWidth;                       //图片显示宽度
     int pixHeight;                      //图片显示高度
     
     QString text;                       //显示文字
     QString image;                      //图像路径
     
+<<<<<<< HEAD
     QRect rectPix;                        //绘图框
         
+=======
+    int angle;                              //rotation angle
+    
+>>>>>>> 291ec0a3dd14e83069641ba3098ad848463efc3f
     QPropertyAnimation *enterAnimation; //进入动画
     QPropertyAnimation *leaveAnimation; //离开动画
     
