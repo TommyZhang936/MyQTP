66fc71b46de139b9b849570ad3951759f821af57
Zhanghq <zhanghq139@139.com>
2016-11-11 17:03:40 +0800

    modified and add

--- imagemorphing/widget.cpp
+++ imagemorphing/widget.cpp
@@ -2,6 +2,7 @@
 #include "ui_widget.h"
 #include <QPainter>
 #include <QTimer>
+#include <QTime>
 #include <QDebug>
 
 Widget::Widget(QWidget *parent) :
@@ -10,26 +11,23 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
     //setWindowState(Qt::WindowMaximized);
-    resize(800, 600);
-
-    imageLenth = 256;
-    morphing = false;
 
     picFrom.load(":/image/from.png");
     picTo.load(":/image/to.png");
-    picGoto.load(":/image/to.png");
-    for(int i = 0; i < imageLenth; i++)
-    {
-        for(int j = 0; j < imageLenth; j++)
-        {
-            rgbFrom[i][j] = picFrom.pixel(i, j);
-            rgbTo[i][j] = picTo.pixel(i, j);
-        }
-    }
+//    picFrom.load(":/image/cherry512.png");
+//    picTo.load(":/image/pear512.png");
+    picGoto = picFrom;    
+    imageLenth = qMin(picFrom.width(), picTo.width());
+    morphing = false;
+    resize(300 + imageLenth,  600);    
+    ui->pushButton->move(width() >> 1, 5);
+    
+    // 预读取图像数据
+    readPic();
 
     timer = new QTimer(this);                   //创建一个定时器
     connect(timer, SIGNAL(timeout()), this, SLOT(imageMorph()));
-    //timer->start(10);                                   //以10ms为一个计时周期
+    timer->start(imageLenth / 10);                                   //以10ms为一个计时周期
 }
 
 Widget::~Widget()
@@ -42,76 +40,104 @@ void Widget::paintEvent(QPaintEvent *)
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
 
-    int x0 = 100;
-    int y0 = 50;
-    QRect rectImage = QRect(x0, y0, imageLenth, imageLenth);
-    QRect rectOut = QRect(x0 - 1, y0 - 1, imageLenth + 2, imageLenth + 2);
-    QRect rect2 = QRect(x0 + imageLenth + 5, y0, imageLenth, imageLenth);
+    int x0 = 5;
+    int y0 = 55;
+    
+    QRect rectOut = QRect(x0 + 265, 54, imageLenth + 2, imageLenth + 2);
+    QRect rectImage = QRect(x0 + 266, 55, imageLenth, imageLenth);
+    QRect rect1 = QRect(x0, y0, 256, 256);
+    QRect rect2 = QRect(x0, y0 + 256 + 5, 256, 256);
     painter.drawRect(rectOut);
-
-    if(morphing)
-    {
-        //painter.drawImage(rectImage, picGoto);
-        painter.drawImage(rect2, picGoto);
-        qDebug() << "sx";
-    }
-    else
-    {
-        painter.drawImage(rectImage, picFrom);
-    }
+    painter.drawImage(rect1, picFrom.scaled(256, 256, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    painter.drawImage(rectImage, picGoto);
+    painter.drawImage(rect2, picTo.scaled(256, 256, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+ 
 }
 
 void Widget::imageMorph()
 {
-    int r, g ,b;
+    QTime time;
+    time.start();
+    int no;
+    int r, g, b, a;
     for(int i = 0; i < imageLenth; i++)
     {
-        int redFrom, greenFrom, blueFrom;
-        int redTo, greenTo, blueTo;
         for(int j = 0; j < imageLenth; j++)
         {
-            redFrom = qRed(rgbFrom[i][j]);
-            redTo = qRed(rgbTo[i][j]);
-            greenFrom = qGreen(rgbFrom[i][j]);
-            greenTo = qGreen(rgbTo[i][j]);
-            blueFrom = qBlue(rgbFrom[i][j]);
-            blueTo = qBlue(rgbTo[i][j]);
-
-            if(redFrom != redTo)
+            no = i * imageLenth + j;
+            //Qvector默认使用隐式共享，可以用setSharable改变其隐式共享。
+            //使用non-const操作和函数将引起深拷贝。即是Copy后地址不一样，是两个不同的量
+            //at()比operator[](),快，因为它不进行深拷贝   Qvector取值都会检查越界问题
+            if(yzColors.at(no).eq)
             {
-                r = (redFrom > redTo) ? redFrom - 1 : redFrom + 1;
+                r = (qAbs(yzColors.at(no).rf - yzColors.at(no).rt) > mStep) ? yzColors.at(no).rf + yzColors.at(no).r * mStep : yzColors.at(no).rt;                
+                g = (qAbs(yzColors.at(no).gf - yzColors.at(no).gt) > mStep) ? yzColors.at(no).gf + yzColors.at(no).g * mStep : yzColors.at(no).gt;
+                b = (qAbs(yzColors.at(no).bf - yzColors.at(no).bt) > mStep) ? yzColors.at(no).bf + yzColors.at(no).b * mStep : yzColors.at(no).bt;
+                a = (qAbs(yzColors.at(no).af - yzColors.at(no).at) > mStep) ? yzColors.at(no).af + yzColors.at(no).a * mStep : yzColors.at(no).at;     
+                QRgb pixGoto = qRgba(r, g, b, a);
+                picGoto.setPixel(i, j, pixGoto);
             }
-            if(greenFrom != greenTo)
-            {
-                g = (greenFrom > greenTo) ? greenFrom - 1 : greenFrom  + 1;
-            }
-            if(blueFrom != blueTo)
-            {
-                b = (blueFrom > blueTo) ? blueFrom - 1 : blueFrom + 1;
-            }
-            //qDebug() << m << QString("%1 , %2 = ").arg(i).arg(j) << r << g << b;
-            QRgb pixGoto = qRgb(r, g, b);
-            picGoto.setPixel(i, j, pixGoto);
-        }
+        }            
     }
-    qDebug() <<  " = "  << morphing << r << g << b;
+    qDebug() <<  time.elapsed() <<" ms";
     update();
-
-
-    //morphing = false;
+    mStep = (mStep + 1) % 256;
 }
 
+//按钮交换From和To图片
 void Widget::on_pushButton_clicked()
 {
     if(!morphing)
     {
         morphing = true;
-        timer->start(10);                                   //以10ms为一个计时周期
+        picGoto = picTo;
+        picTo = picFrom;
+        picFrom = picGoto; 
     }
     else
     {
         morphing = false;
-        timer->stop();
-        update();
+        picGoto = picFrom;
+        picFrom = picTo;
+        picTo = picGoto;
     }
+    readPic();
+}
+
+void Widget::readPic()
+{
+    QColor from, to;
+    DOTCOLOR newColor;       
+    yzColors.clear();
+    for(int i = 0; i < imageLenth; i++)
+    {
+        for(int j = 0; j < imageLenth; j++)
+        {
+            from = picFrom.pixelColor(i, j);
+            to = picTo.pixelColor(i, j);            
+            if(from != to)
+            {
+                newColor.eq = true;
+                newColor.rf = from.red();
+                newColor.rt = to.red();
+                newColor.r = (newColor.rf == newColor.rt) ? 0 : (newColor.rf > newColor.rt) ? -1 : 1 ;                
+                newColor.gf = from.green();
+                newColor.gt = to.green();
+                newColor.g = (newColor.gf == newColor.gt) ? 0 : (newColor.gf > newColor.gt) ? -1 : 1 ;
+                newColor.bf = from.blue();
+                newColor.bt = to.blue();
+                newColor.b = (newColor.bf == newColor.bt) ? 0 : (newColor.bf > newColor.bt) ? -1 : 1 ;
+                newColor.af = from.alpha();
+                newColor.at = to.alpha();
+                newColor.a = (newColor.af == newColor.at) ? 0 : (newColor.af > newColor.at) ? -1 : 1 ;
+            }
+            else
+            {
+                newColor.eq = false;            
+                //其余数据不关心也用不到，所以可以不用管
+            }
+            yzColors.push_back(newColor);
+        }
+    }
+
 }
--- imagemorphing/widget.h
+++ imagemorphing/widget.h
@@ -7,6 +7,15 @@ namespace Ui {
 class Widget;
 }
 
+// 图片颜色结构体 
+struct DOTCOLOR                                       
+{  
+    bool eq;                                   // 颜色值是否一致
+    int r, g, b, a;                             // 颜色值变化因子 0 1 -1
+    int rf, gf, bf, af;                        // From 颜色值
+    int rt, gt, bt, at;                        // To 颜色值
+};
+
 class Widget : public QWidget
 {
     Q_OBJECT
@@ -23,29 +32,22 @@ private slots:
     void imageMorph();
 
 private:
-
-
+    void readPic();
+    
 private:
     Ui::Widget *ui;
 
-    int imageLenth;
-    bool morphing;
+    int imageLenth;                       // 图片宽度（默认长宽一致）
+    bool morphing;                       // 变化方向，true正 false负
+    int mStep;                              // 当前步长
 
     QTimer *timer;
 
-    QImage picFrom;
-    QImage picTo;
-    QImage picGoto;
-    QRgb rgbFrom[256][256];
-    QRgb rgbTo[256][256];
-    QRgb rgbGoto[256][256];
-//    int redFrom[256][256];
-//    int greenFrom[256][256];
-//    int blueFrom[256][256];
-//    int redTo[256][256];
-//    int greenTo[256][256];
-//    int blueTo[256][256];
-
+    QImage picFrom;                     // From图片
+    QImage picTo;                         // To图片
+    QImage picGoto;                     // 中间图片
+    
+    QVector<DOTCOLOR> yzColors;
 };
 
 #endif // WIDGET_H
